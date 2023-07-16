db9ee9ae51a7c90536ce6d7944c4e22910bcbd3f
betheng <131614496+betheng@users.noreply.github.com>
2023-07-16 18:32:44 -0400

    add new naming convention and file write structure so new logos are added but not overwriting previous

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const {setShape} = require('./lib/setShapesColors.js')
 
 // creates svg file
 function createLogo(response) {
+    let fileIndex = 1;
+    let newFileName = `${fileName.slice(0, -4)}_${fileIndex}.svg`;
+  
+    while (fs.existsSync(newFileName)) {
+      fileIndex++;
+      newFileName = `${fileName.slice(0, -4)}_${fileIndex}.svg`;
+    }
+  
     const svg = setShape(response);
-    fs.writeFile(fileName, svg, ()=> console.log('Please look under examples directory to find your generated .svg logo!'));
-}
-
+    fs.writeFile(newFileName, svg, (err) => {
+      if (err) {
+        console.error(`Error creating ${newFileName}:`, err);
+      } else {
+        console.log(`Logo created: ${newFileName}`);
+      }
+    });
+  }
 // initialize logo questions using inquirer
 function init() {
     inquirer 
